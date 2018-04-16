bca80f973bec848b4c0676a063dd90af9a9f4761
Sean Jun <seanjun21@gmail.com>
2018-04-16 12:48:22 -0700

    setup heroku

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
