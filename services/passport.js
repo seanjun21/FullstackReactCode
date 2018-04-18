8d883aa7150f36eef31be39983e54ddac667cf36
Sean Jun <seanjun21@gmail.com>
2018-04-17 18:23:11 -0700

    adding MongoDB to handle authentication

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
 passport.use(new GoogleStrategy(
   {
@@ -9,8 +22,14 @@ passport.use(new GoogleStrategy(
     callbackURL: '/auth/google/callback',
   },
   (accessToken, refreshToken, profile, done) => {
-    console.log('access token: ', accessToken);
-    console.log('refresh token: ', refreshToken);
-    console.log('profile: ', profile);
+    User.findOne({ googleId: profile.id }).then((existingUser) => {
+      if (existingUser) {
+        // we already have a record with the given profile ID
+        done(null, existingUser);
+      } else {
+        // we don't have a user record with this ID, make a new record
+        new User({ googleId: profile.id }).save().then(user => done(null, user));
+      }
+    });
   },
 ));
