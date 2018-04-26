6b32a54b5808e604eb51da9bdb77b2667155a576
Sean Jun <seanjun21@gmail.com>
2018-04-25 23:53:24 -0700

    redux review and setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
-ReactDom.render(<App />, document.querySelector('#root'));
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root'),
+);
