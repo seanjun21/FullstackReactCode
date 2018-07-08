3105bbee1050136a249f8c22145975629e498f51
Sean Jun <seanjun21@gmail.com>
2018-07-08 02:49:21 -0700

    update npm and mongo setting

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
@@ -22,5 +22,3 @@ require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-// testing git
