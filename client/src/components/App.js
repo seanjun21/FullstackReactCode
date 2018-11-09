c6d3b11f71fba61e0e9e7ad052ee8ebbd04a50c9
Sean Jun <seanjun21@gmail.com>
2018-11-09 15:03:16 -0700

    Update up to always visible components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-const App = () => <div>Hi There!</div>;
+const Header = () => (<h2>Header</h2>);
+const Dashboard = () => (<h2>Dashboard</h2>);
+const SurveyNew = () => (<h2>SurveyNew</h2>);
+const Landing = () => (<h2>Landing</h2>);
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={Landing} />
+      </div>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
